f13cee73f5a0305519ce9bb8eaa06085bec195b5
Katie Arriaga M <katiearriaga@live.com>
2019-02-08 18:32:23 -0600

    Update gulp file to run nucleus task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var notify = require('gulp-notify');
+var run = require('gulp-run');
+
 
 function handleErrors(e) {
 	var args = Array.prototype.slice.call(arguments);
@@ -13,7 +15,9 @@ function handleErrors(e) {
 	}).apply(this, args);
 	this.emit('end'); // Keep gulp from hanging on this task
 }
-
+function nucleus() {
+	return run('nucleus --config config.nucleus.json').exec();
+}
 function styles() {
 	return gulp.src('./src/css/**/*.scss')
 		// .pipe(plumber())
@@ -22,14 +26,23 @@ function styles() {
 		// .pipe(browserSync.reload({stream:true}));
 }
 
+// function nucleus() {
+// 	return gulp.src('./src/css/**/*.scss')
+// 		// .pipe(plumber())
+// 		.pipe(sass.sync({outputStyle: 'expanded'}).on('error', handleErrors))
+// 		.pipe(gulp.dest('./tmpbuild'))
+// 		// .pipe(browserSync.reload({stream:true}));
+// }
+
 function watch() {
-	gulp.watch("src/css/*.scss", gulp.series(styles));
+	gulp.watch("src/css/*.scss", gulp.series(styles, nucleus));
 }
 
 var build = gulp.series(styles, watch);
 
 exports.styles = styles;
 exports.watch = watch;
+exports.nucleus = nucleus;
 exports.build = build;
 exports.default = build;
 
